12f71b540515d6aea96219a7c4540cd99b5a53d9
Szymon Sroka <sroka.a.szymon@outlook.com>
2025-11-08 12:38:40 +0100

    refactor: :art:

--- parser.py
+++ parser.py
@@ -1,22 +1,22 @@
 from tkinter import *
 from tkinter import filedialog
 import xml.etree.ElementTree as ET
+
 # Files.
 from xml_load import xml_root_load, xml_load_error
 
 # Root.
 root = Tk()
-app_title = 'XML file parser '
+app_title = "XML file parser "
 root.title(app_title)
+
 
 #################################################################################################### Import file (xml).
 def open_file_dialog():
     root.filename = filedialog.askopenfilename(
-        initialdir='/',
-        title='Select xml file',
-        filetypes=[
-            ("xml files (*.xml)", "*.xml")
-        ]
+        initialdir="/",
+        title="Select xml file",
+        filetypes=[("xml files (*.xml)", "*.xml")],
     )
 
     # Window name change.
@@ -33,24 +33,23 @@ def open_file_dialog():
     except ET.ParseError:
         xml_load_error(root)
 
+
 #################################################################################################### Create menu.
 menuBar = Menu(root)
 
 # Adding file menu.
 fileMenu = Menu(menuBar, tearoff=0)
-menuBar.add_cascade(label='File', menu=fileMenu)
-fileMenu.add_command(label='Open xml file', command=open_file_dialog)
+menuBar.add_cascade(label="File", menu=fileMenu)
+fileMenu.add_command(label="Open xml file", command=open_file_dialog)
 fileMenu.add_separator()
-fileMenu.add_command(label='Exit', command=root.quit)
+fileMenu.add_command(label="Exit", command=root.quit)
 # Adding help menu.
 helpMenu = Menu(menuBar, tearoff=0)
-menuBar.add_cascade(label='Help', menu=helpMenu)
-helpMenu.add_command(label='GitHub')
+menuBar.add_cascade(label="Help", menu=helpMenu)
+helpMenu.add_command(label="GitHub")
 
 #################################################################################################### Root.
 root.config(menu=menuBar)
-root.geometry(
-    '1024x768+20+20'
-)
+root.geometry("1024x768+20+20")
 
 root.mainloop()
